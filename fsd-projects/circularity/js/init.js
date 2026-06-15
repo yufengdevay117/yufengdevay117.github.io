11e25130013b7c0ba344ebfcf851a1b777fd5cd9
yufengdevay117 <yufengdevay117@gmail.com>
2026-06-15 18:08:02 +0000

    completed bouncing box project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,12 +20,15 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+     var circle;
+     var circle = []
 
         // TODO 2 : Create a function that draws a circle 
-        
-
+     function drawCircle
+     circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+     physikz.addRandomVelocity(circle, canvas, 5, 5);
+     view.addChild(circle);
+     circles.push(circle);
 
         // TODO 3 : Call the drawCircle() function
 
